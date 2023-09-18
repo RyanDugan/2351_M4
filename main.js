822bf4f87a2a06f7f2d57cc45c974306e1861cae
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-09-18 13:59:12 -0500

    tested js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("Hello NVC!");
+const randomQuestion = `Here is the question right here maybe yes?`;
+console.log(randomQuestion);
+
+const name = `Ryan`;
+
+console.log(`Whaddup, ${name}?`);
