58b53cf5f0aab9e71090f4cd2bb71977f3caf716
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-09-19 16:21:48 -0500

    modified concatenated string variable

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ console.log(randomQuestion);
 const school = `NVC`;
 console.log(`Hello, ${school}!`);
 
+let quanShells = 77;
 const seaShells = `seashells`;
 const seaShore = `seashore`;
+let timeClock = 6;
+const oClock = `o'clock`;
 document.getElementById(
   `test1`
-).innerHTML = `She sells ${seaShells} by the ${seaShore}!`;
+).innerHTML = `She sells ${quanShells} ${seaShells} by the ${seaShore} at ${timeClock} ${oClock}!`;
