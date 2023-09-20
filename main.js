b104311662c097736df471c43565eee859d2213c
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-09-19 21:26:33 -0500

    updated number method text

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ let y = 696.304053463;
 let a = 60703.3453367522442;
 let n = 58.5335;
 document.getElementById(`test5`).innerHTML =
-  `Each of these numbers are rounded to 0, 3, 6, and 1 decimal places.` +
+  `Each of these numbers are rounded and given a specific decimal place to display.` +
   "<br><br>" +
   r.toFixed(0) +
   "<br>" +
