5d48a5e0379259cf48ba38b7d76479e91bc8f3bd
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-09-19 21:21:46 -0500

    added methods

--- main.js
+++ main.js
@@ -12,3 +12,23 @@ const oClock = `o'clock`;
 document.getElementById(
   `test1`
 ).innerHTML = `She sells ${quanShells} ${seaShells} by the ${seaShore} at ${timeClock} ${oClock}!`;
+
+//number method
+let r = 12.503496;
+let y = 696.304053463;
+let a = 60703.3453367522442;
+let n = 58.5335;
+document.getElementById(`test5`).innerHTML =
+  r.toFixed(0) +
+  "<br>" +
+  y.toFixed(3) +
+  "<br>" +
+  a.toFixed(6) +
+  "<br>" +
+  n.toFixed(1);
+
+//string method
+let favFood = `I can be indecisive. My favorite food (I think) is Italian!`;
+let actualFavFood = favFood.replace(`Italian`, `Thai`);
+console.log(favFood, actualFavFood);
+document.getElementById(`test4`).innerHTML = actualFavFood;
