bfe0ec7f80dd2cb48722ec6451f0bcba33b34df1
Ryan Dugan <70173704+RyanDugan@users.noreply.github.com>
2023-09-21 12:52:25 -0500

    modified backtick to quotes outside interpolation

--- main.js
+++ main.js
@@ -11,76 +11,74 @@ const seaShore = `seashore`;
 let timeClock = 6;
 const oClock = `o'clock`;
 document.getElementById(`test1`).innerHTML =
-  `She sells` +
-  " " +
+  "She sells " +
   quanShells +
   " " +
   seaShells +
   " " +
-  `by the` +
-  " " +
+  "by the " +
   seaShore +
   " " +
-  `at` +
-  " " +
+  "at " +
   timeClock +
   " " +
   oClock;
 
-//bonus - string interpolation is MUCH cleaner!
+//string interpolation is MUCH cleaner!
 document.getElementById(
-  `test1.5`
+  "test1.5"
 ).innerHTML = `She sells ${quanShells} ${seaShells} by the ${seaShore} at ${timeClock} ${oClock}!`;
+
 //if conditional statement
 let dice = 12; //plug in the dice result from a d20 roll here!
 
 if (dice >= 13) {
-  result = `Success! Luck was on your side!`;
+  result = "Success! Luck was on your side!";
 } else if (dice >= 10) {
-  result = `Slight failure! So close!`;
+  result = "Slight failure! So close!";
 } else {
-  result = `Failure! Better luck next time!`;
+  result = "Failure! Better luck next time!";
 }
-document.getElementById(`test2`).innerHTML = result;
+document.getElementById("test2").innerHTML = result;
 
 document.getElementById(
-  `test2.2`
+  "test2.2"
 ).innerHTML = `You need to roll a 13 or higher. You rolled a ${dice} on a d20!`;
 
 //switch statement
-let passivePerception = `Rascal`; //write in one of the player character names to see their passive perception score!
+let passivePerception = "Rascal"; //write in one of the player character names to see their passive perception score!
 
 switch (passivePerception) {
-  case `Theo`:
-    document.getElementById(`test3`).innerHTML = 9;
+  case "Theo":
+    document.getElementById("test3").innerHTML = 9;
     break;
-  case `Gnar`:
-  case `Rascal`:
-    document.getElementById(`test3`).innerHTML = 13;
+  case "Gnar":
+  case "Rascal":
+    document.getElementById("test3").innerHTML = 13;
     break;
-  case `Caia`:
-    document.getElementById(`test3`).innerHTML = 14;
+  case "Caia":
+    document.getElementById("test3").innerHTML = 14;
     break;
-  case `Otto`:
-    document.getElementById(`test3`).innerHTML = 16;
+  case "Otto":
+    document.getElementById("test3").innerHTML = 16;
     break;
-  case `Kiba`:
-    document.getElementById(`test3`).innerHTML = 17;
+  case "Kiba":
+    document.getElementById("test3").innerHTML = 17;
 }
 
 //string method
-let favFood = `I can be indecisive. My favorite type of food is...Italian!`;
-let actualFavFood = favFood.replace(`Italian`, `Thai`);
+let favFood = "I can be indecisive. My favorite type of food is...Italian!";
+let actualFavFood = favFood.replace("Italian", "Thai");
 console.log(favFood, actualFavFood);
-document.getElementById(`test4`).innerHTML = actualFavFood;
+document.getElementById("test4").innerHTML = actualFavFood;
 
 //number method
 let r = 12.503496;
 let y = 696.304053463;
 let a = 60703.3453367522442;
 let n = 58.5335;
-document.getElementById(`test5`).innerHTML =
-  `Each of these numbers are rounded and given a specific decimal place to display.` +
+document.getElementById("test5").innerHTML =
+  "Each of these numbers are rounded and given a specific decimal place to display." +
   "<br><br>" +
   r.toFixed(0) +
   "<br>" +
